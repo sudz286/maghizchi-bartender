99f29d723b6b18bd2add054d667e3cd71b79b261
Sudharsan Janardhanan <sudharsanjanardhanan@Sudharsans-Laptop.local>
2025-09-11 13:36:55 -0700

    modifed menu item fade effect

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,17 @@ function App() {
       <Header />
       <MenuItem 
         title="tang dynasty" 
-        description="a vibrant indian-inspired cocktail that brings back childhood memories with a boozy twist."
+        description="indian-inspired cocktail that brings back your childhood memories with a boozy twist."
         ingredient_list="vodka, white rum, rasna mix, lime juice, mint leaves, orange slice, lime wheel"
       />
       <MenuItem 
         title="madras mule" 
-        description="a sophisticated south indian coffee house meets cocktail lounge. rich coffee concentrate blends with premium spirits and cream."
+        description="south indian coffee house meets cocktail lounge. what could possibly go wrong?"
         ingredient_list="vodka, kahlua, coffee, heavy cream, simple syrup"
       />
       <MenuItem 
         title="*mango lassi" 
-        description="a creamy, traditional Indian yogurt-based drink that's naturally refreshing and cooling. can be made alcoholic on request ;)"
+        description="a traditional, creamy Indian yogurt-based drink that's naturally refreshing and cooling. psst, it can be made alcoholic on request ;)"
         ingredient_list="fresh mango lassi, yogurt, natural sweeteners, traditional indian spices"
       />
       <Footer />
